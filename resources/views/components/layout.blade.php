c83f53be17da6a8d72e6b5138b9094b565b13018
Mert Hurturk <mert.hurturk@gmail.com>
2023-10-13 12:45:27 +0300

    Added fathom analytics

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -4,6 +4,10 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     @vite('resources/css/app.css')
+
+    <!-- Fathom - beautiful, simple website analytics -->
+    <script src="https://cdn.usefathom.com/script.js" data-site="JXHAIISH" defer></script>
+    <!-- / Fathom -->
 </head>
 <body class="p-10 bg-zinc-100 dark:bg-zinc-800">
 
