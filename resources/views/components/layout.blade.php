220460c6c6f8b0974b6947e33dd0b29f1c395fb2
Mert Hurturk <mert.hurturk@gmail.com>
2023-10-13 12:43:03 +0300

    Added about text

---
+++ resources/views/components/layout.blade.php
@@ -0,0 +1,13 @@
+<!doctype html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    @vite('resources/css/app.css')
+</head>
+<body class="p-10 bg-zinc-100 dark:bg-zinc-800">
+
+{{ $slot }}
+
+</body>
+</html>
--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -1,3 +1,6 @@
-<div>
-    Hello world
-</div>
+<x-layout>
+    <article class="prose dark:prose-invert prose-zinc mx-auto prose-a:text-violet-600">
+        <p class="lead">Hello! <strong>I'm Mert Hürtürk</strong>, a versatile software developer living in the scenic city of Bodrum. My journey is sparked by creativity and a non-stop growth mindset, exploring software, music, art, and writing.</p>
+        <p>I co-founded <a href="https://octeth.com" target="_blank">Octeth</a> and <a href="https://upcoach.com" target="_blank">upcoach</a>, embracing the roles of developer, designer, and tech lead. Though I started studying Visual Communication Design, I ventured into self-learning to broaden my horizons. I believe in continuous improvement, giving back, and empowering others. When I'm not coding, I love playing the guitar, drawing, or getting lost in thought, always on a quest for new inspiration and ways to grow.</p>
+    </article>
+</x-layout>
