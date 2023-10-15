c10da7fe1f95972e05ae2086655bbf05dbfa502c
Mert Hurturk <mert.hurturk@gmail.com>
2023-10-15 13:34:56 +0300

    Published my first weekly review

---
+++ resources/views/entries/1697360311-weekly--w42-2023.blade.php
@@ -0,0 +1,37 @@
+<h1>W42 2023</h1>
+<p class="lead">2023 Oct 16 - 22 &bullet; Year progress: 78%</p>
+
+<h3>Highlights</h3>
+<ul>
+    <li>Successfully implemented the weekly planning routine from <a href="https://en.wikipedia.org/wiki/The_7_Habits_of_Highly_Effective_People" target="_blank">The 7 Habits of Highly Effective People</a> for the first time.</li>
+    <li>Discovered <a href="https://www.youtube.com/channel/UCC7yp_9Mq0MX187pLNfTOcw" target="_blank">Monika Roscher Bigband</a> and instantly became a fan. Their track <a href="https://www.youtube.com/watch?v=Iwq0Fd8qYKU" target="_blank">8 Prinzessinnen</a> is a delightful fusion of math-rock, prog-jazz, avant-pop, and experimental electronics. They've quickly climbed to the top of my recent favorites list.</li>
+    <li>Even though I love discovering new note-taking apps and experimenting with them, I always come back to Apple Notes. Despite its quirks and lack of features, it feels like home.</li>
+</ul>
+
+<h3>During the week</h3>
+<ul>
+    <li>Took a 3-day trip to Istanbul to visit my parents.</li>
+    <li>Shared the second 4-week review of our novel product delivery method at <a href="https://upcoach.com" target="_blank">upcoach</a> with the team. It's heartening to witness the positive impact of the new approach.</li>
+    <li>Started prepping for <a href="https://upcoach.com" target="_blank">upcoach</a>'s annual retreat happening next week at <a href="https://www.instagram.com/dunyam_village/" target="_blank">Dunyam Event Space Bodrum</a>, brainstorming on ideas and presentations.</li>
+</ul>
+
+<h3>Weekend</h3>
+<ul>
+    <li>Made headway on Event Sage, an open-source app and user analytics package tailored for Laravel.</li>
+    <li>Advanced this website to a stage ready for publishing and sharing.</li>
+    <li>Enjoyed a family breakfast on Sunday at a cozy spot, a cherished weekly tradition with my wife and daughters.</li>
+    <li>Completed another read of
+        <a href="https://en.wikipedia.org/wiki/The_7_Habits_of_Highly_Effective_People" target="_blank">The 7 Habits of Highly Effective People</a> by Stephen R. Covey, continually discovering new nuggets of wisdom despite it being my third time through.</li>
+</ul>
+
+<h3>Review</h3>
+
+<p>Although I made good progress on various tasks, I didn't complete the most important task during the week. This led to working extra hours over the weekend, which was stressful. I will do better balancing quadrant one and quadrant two tasks<sup><a href="#note1">1</a></sup> the next week.</p>
+
+<x-blockquote author="William Shakespeare">
+    There is nothing either good or bad but thinking makes it so.
+</x-blockquote>
+
+<section class="text-xs opacity-50">
+    <p id="note1">1. <a href="https://en.wikipedia.org/wiki/Time_management#The_Eisenhower_Method" target="_blank">The Eisenhower Method</a></p>
+</section>
