621f1e4bc6bb68160b75a265d8109ff66ed21118
Mert Hurturk <mert.hurturk@gmail.com>
2023-10-18 11:47:37 +0300

    Publish procrastination post

---
+++ resources/views/entries/1697624010-reflections--overcoming-my-procrastination.blade.php
@@ -0,0 +1,72 @@
+<h1>Overcoming My Procrastination</h1>
+
+<x-tldr>I reflect on my experience with procrastination, its underlying causes, and the tools I've used to combat it — from maintaining control and reflecting on values, to sensory aids and time constraints. Also, I highlight potential tools I'm keen to explore in the future.</x-tldr>
+
+<p class="lead">No matter how much I study and practice productivity, or how disciplined I become, from time to time it just happens… Goblins from the realm of procrastination emerge and pin me down.</p>
+
+<x-image :src="asset('assets/images/goblins-from-procrastination-realm.webp')">
+    <x-ai-prompt label="Image generated with">High-resolution watercolor painting showcasing a man with glasses, a black t-shirt, and a short beard, seated at a desk, deeply engrossed in his laptop. The surrounding environment is awash with deep blues, purples, and touches of gray. Goblins, with their mischievous smiles and cunning eyes, emerge from the engulfing darkness, gripping his head and shoulders, keeping him restrained. The setting feels reminiscent of a spine-chilling children's story.</x-ai-prompt>
+    <a href="https://chat.openai.com/chat" target="_blank">OpenAI, ChatGPT</a>, 2023.
+</x-image>
+
+<p>I've come to realize that my occasional dives into procrastination are not random lapses in discipline. Instead, they serve as indicators of underlying challenges.</p>
+
+<ul>
+    <li>Unclear tasks at hand.</li>
+    <li>Tasks not aligning with my values or goals.</li>
+    <li>A lack of energy.</li>
+    <li>Absence of purpose.</li>
+    <li>Lack of structured systems.</li>
+    <li>Uncertainty about how to tackle the task.</li>
+    <li>Overwhelming task size.</li>
+    <li>Emotions tied to the task.</li>
+    <li>Perfectionism, or expecting too much from myself.</li>
+</ul>
+
+<p>In essence, procrastination is merely an obstacle, a challenge waiting to be overcome.</p>
+
+<x-blockquote author="Marcus Aurelius">
+    The mind adapts and converts to its own purposes the obstacle to our acting. The impediment to action advances action. What stands in the way becomes the way.
+</x-blockquote>
+
+<h2>My Tools for Dealing with Procrastination</h2>
+
+<p>I've had my share of procrastination over the years. But with time, I found some tools that help me handle it better:</p>
+
+<h3>Sense of Control</h3>
+<p>I prefer to maintain a certain degree of control over my environment. Tidying up and organizing my desk, be it a coffee table, dinner table, or my office desk, gives me a sufficient sense of control to start with, which I find to be an excellent motivator. Simple actions, like aligning my laptop perfectly with the edge of the table or arranging my notebook, phone, laptop, and charging cables so they seem to snap into place, are easy, effortless, and make a big difference for me.</p>
+
+<h3>Sense of Purpose</h3>
+<p>Whenever I feel resistant to a task and I'm mindful of it, I take a moment to pause and reflect on my core values. Often, one of these values offers a solution. For instance, my value of "Giving Back" encourages me to see tasks as learning opportunities. By documenting and sharing my journey, I find a greater purpose in what I'm doing. It becomes less about me and more about the value I can bring to others. Writing this article is a prime example.</p>
+
+<h3>Sensory Companion</h3>
+
+<p>I’ve been using
+    <a href=https://endel.io" target="_blank">Endel</a> for about a year, and it's been a game-changer for me. It offers personalized soundscapes designed for focus, relaxation, and sleep. While I've primarily used the focus features, I can attest to their effectiveness. If you're curious about how it works, their website delves into
+    <a href="https://endel.io/science" target="_blank">the science behind it</a>. I find that initiating the 'deep work' scenario and setting a timer for at least 30 minutes significantly enhances my concentration. It's worth noting that for me the experience is optimal when using headphones.
+</p>
+
+<h3>Time Constraints</h3>
+
+<p>I've found that setting a timer or scheduling tasks or events immediately after a work block in my calendar can transform an intimidating task into a thrilling race against time. It's also important to mention that, for me, only countdown timers do the trick. Introducing this time constraint eliminates distractions, helping me to zero in solely on the task at hand.</p>
+
+<x-blockquote author="Jerry Seinfeld on Tim Ferriss podcast ep. 485, 33:56" href="https://youtu.be/yNTmFORn3xQ?si=gkIdKcVwIATPK45x&t=2036">
+    It’s like you’re going to hire a trainer to get in shape, and he comes over, and you go, “How long is the session?” And he goes, “It’s open-ended.” Forget it. I’m not doing it. It’s over right there. You’ve got to control what your brain can take.
+</x-blockquote>
+
+<h3>Note Taking</h3>
+
+<p>Having a notebook always open (or using Apple Notes) while working is essential for me. It acts as an inbox where I can quickly jot down any thoughts that come to mind, ensuring they won't be forgotten and I’m not distracted. Once my focus session ends, I revisit these notes. I prefer using a paper notebook for this purpose because of its simplicity. Without the distractions of organizational features, I can just dump everything in swiftly without overthinking.</p>
+
+<h2>Other Tools to Explore</h2>
+
+<p>While I've listed some of the tools that have been instrumental in my journey so far, there are several others that have caught my attention and I'm keen to explore further:</p>
+
+<ul>
+    <li><strong>Do Nothing Else:</strong> Inspired by Jerry Seinfeld and Neil Gaiman, this method is all about committing to a task during a designated time. Sit down, start the task, and <i>you're not permitted to leave or engage in any other activities</i>, including checking social media. If you find yourself unable to focus on the task, the rule remains: simply sit without doing anything else until the allotted time is up.</li>
+    <li><strong>Gamification:</strong> As a fan of board games and role-playing games, I've always toyed with the idea of blending these elements into my productivity system. Although I'm still brainstorming the specifics, I imagine a system where tasks completed equate to experience points, levels can be achieved, and challenges and quests become daily objectives. The addition of customizable avatars and a potential narrative structure could make mundane tasks more engaging. While there are apps that offer similar features, I believe there's room for something simpler that can seamlessly integrate into other workflows.</li>
+</ul>
+
+<p>This article has been a chance for me to put my thoughts on paper, to clarify the methods and tools I've used over the years to address procrastination. It's a journey that I'm still on, and writing about it helps me understand it better. If any of these insights resonate with you or help you in any way, that's a bonus. But at its core, this is just a snapshot of my ongoing exploration into understanding and improving my own productivity habits. Here's to many more discoveries, both for me and for anyone else on a similar journey.</p>
+
+<x-join-conversation href="https://twitter.com/merthurturk/status/1713509120496508986" />
