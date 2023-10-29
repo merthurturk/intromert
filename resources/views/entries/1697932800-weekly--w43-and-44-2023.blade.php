f7894a2737bc07395fb93e78b01a496bc48a7799
Mert Hurturk <mert.hurturk@gmail.com>
2023-10-29 18:06:05 +0300

    Published w43 and 44 week updates

---
+++ resources/views/entries/1697932800-weekly--w43-and-44-2023.blade.php
@@ -0,0 +1,27 @@
+<h1>W43 and 44 2023</h1>
+<p class="lead">2023 Oct 16 - 29 &bullet; Year progress: 82%</p>
+
+<h3>Highlights</h3>
+<ul>
+    <li>Published my first article on this website: <a href="/entries/reflections/overcoming-my-procrastination">Overcoming My Procrastination</a>. It's the start of a new habit I want to build: writing detailed reflections.</li>
+    <li>Went to the gym three times, which is pretty good considering my Istanbul trip and team retreat.</li>
+    <li>Caught up with the Upcoach team in Bodrum from October 22 to 28. So good to see everyone face-to-face again!</li>
+    <li>100th Birthday of the <a href="https://en.wikipedia.org/wiki/Turkiye" target="_blank">Turkish Republic</a></li>
+    <li>Launched a new <a href="/quotes">Quotes</a> section and a <a href="/quotes/random">Random Quote</a> feature on my site. Every time I update the site, it automatically pulls in quotes from my entries. Planning to do the same to the external links I share.</li>
+</ul>
+
+<h3>Weekend</h3>
+<ul>
+    <li>Had an awesome family breakfast and watched the <a href="https://www.soloturk.tsk.tr" target="_blank">SOLOTÜRK</a> team do their thing over Bodrum Castle.</li>
+    <li>Cheered on my daughter in her first volleyball match with her school team. Go team!</li>
+</ul>
+
+<h3>Review</h3>
+
+<p>It's been a socially packed couple of weeks. Being around lots of people can wear me out, but I'm learning to be okay with that. Looked at my to-do list and realized not much matched up with my big goals. Time to refocus on what really matters.</p>
+
+<x-blockquote author="Julia Cameron, The Artist's Way: A Spiritual Path to Higher Creativity">
+    Progress, not perfection, is what we should be asking of ourselves.
+</x-blockquote>
+
+<x-join-conversation href="https://twitter.com/merthurturk/status/1713509120496508986"/>
