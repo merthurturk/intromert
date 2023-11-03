62bf927da93a59b8eaf6251f9fb49048ea894f31
Mert Hurturk <mert.hurturk@gmail.com>
2023-11-03 23:17:06 +0300

    Published "Read Later Reimagined" entry

---
+++ resources/views/entries/1699039069-tips-and-tricks--read-later-reimagined.blade.php
@@ -0,0 +1,46 @@
+<h1>Read Later Reimagined</h1>
+
+<x-tldr>Revamped my reading list with due dates via Apple Reminders, ensuring I read or reassess within 30 days—no more endless, guilt-inducing backlog. There's also a download link for the Apple Shortcut I've created for this at the end.</x-tldr>
+
+<p class="lead">I used to stockpile 'read-laters' like there was no tomorrow, but let's be real — 'later' often turned into 'never'. That pile was a mix of everything I wanted to dive into, someday. But that someday rarely came, and honestly, it started to bug me.</p>
+
+<x-image :src="asset('assets/images/pile-of-books-read-later.png')">
+    <x-ai-prompt label="Image generated with">A wide photo in a 3:2 aspect ratio showing a cluttered desk with a towering pile of books, a smartphone showing a 'To-Do List' app with articles listed as tasks, and due dates clearly visible. A desk lamp shines warmly on the arrangement, and a wall calendar features prominently with deadlines for reading tasks clearly circled.</x-ai-prompt>
+    <a href="https://chat.openai.com/chat" target="_blank">OpenAI, ChatGPT</a>, 2023.
+</x-image>
+
+<p>So, I looked at how I was saving these links. Bookmarks? They felt old-school, unnecessary when my browser knows me almost better than I know myself. A few letters typed on the address bar and I'm where I need to be, no saving or structure required.</p>
+
+<p>Then there's the 'save for later' trend. Those apps are cool and I've used many of them in the past, but they just didn't fit into my flow. My 'later' list was getting lost in the shuffle of daily life.</p>
+
+<p>That's when it clicked &mdash; why not drop these reads right into Apple Reminders? I gave them due dates, treating them like any other task. It made me think — is this article really worth my time? If yes, it got a spot in there.</p>
+
+<p>Here's how I've nailed it down:</p>
+
+<ul>
+    <li><strong>For bookmarks:</strong> I skip 'em. Apple Safari is smart enough.</li>
+    <li>
+        <strong>For 'save for later':</strong>
+        <ol>
+            <li>I send interesting finds to Apple Reminders.</li>
+            <li>Add #readlater tag.</li>
+            <li>Pick a random due date within the next month.</li>
+        </ol>
+    </li>
+</ul>
+
+<p>Some benefits I got from this new system:</p>
+
+<ol>
+    <li>Everyday I get 2-3 extra reminders which has links attached to them, much easier to go through compared to an endless backlog of links in a read-later list.</li>
+    <li>Now that I know these links are going to come back at me some time in the next 30 days, I'm being more mindful of what I'm pushing into the list.</li>
+    <li>I have a list of links checked off in Apple Reminders app, which later I can review and reflect on.</li>
+</ol>
+
+<p>And to make things even slicker, I created a Shortcut. Now, saving stuff for later is quick, painless, and actually useful.</p>
+<x-image :src="asset('assets/images/merts-read-later-shortcut.png')">
+    Screenshot of the Apple Shortcut I've created.
+    <a href="https://www.icloud.com/shortcuts/948c75d951ce4017b9bd4196a814369f" target="_blank">&darr; Download this shortcut</a>
+</x-image>
+
+{{--<x-join-conversation href="https://twitter.com/merthurturk/status/1714565071588729056"/>--}}
