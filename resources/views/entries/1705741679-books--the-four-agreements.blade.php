b2a0c21dfadaa548bcad534829da25d34828ad44
Mert Hurturk <mert.hurturk@gmail.com>
2024-01-20 13:01:58 +0300

    Added "The Four Agreements" book notes

---
+++ resources/views/entries/1705741679-books--the-four-agreements.blade.php
@@ -0,0 +1,52 @@
+<h1>The Four Agreements</h1>
+
+<p class="lead">The power of words should be wielded with care and responsibility; personal reactions to external events need not define our inner state; assumptions are often misleading and should be replaced with inquiry and understanding. Additionally, the concept of always giving our best effort, adaptable to the ebb and flow of our capabilities, teaches us that persistence and dedication are vital for mastery and continual self-improvement.</p>
+
+<x-blockquote author="Don Miguel Ruiz, The Four Agreements: A Toltec Wisdom Book">
+    Action is about living fully. Inaction is the way we deny life.
+</x-blockquote>
+
+<x-image :src="asset('assets/images/the-four-agreements.jpeg')">
+    <a href="https://www.goodreads.com/en/book/show/6596" target="_blank">The Four Agreements: A Toltec Wisdom Book, Don Miguel Ruiz</a>.
+</x-image>
+
+<h3>Be Impeccable With Your Word</h3>
+<ul>
+    <li>Impeccable means "without sin". A sin is anything that you do which goes against yourself and universal principles.</li>
+    <li>When you're impeccable you take responsibility for your actions.</li>
+    <li>You have the power to create through your words, bad or good.</li>
+    <li>With words you can create the most beautiful dream or destroy everything around you.</li>
+    <li>Don't use the word to curse, to blame, to find guilt and to destroy.</li>
+    <li>Don't spread your personal poison.</li>
+    <li>Whenever we hear an opinion and believe it, we make an agreement and it becomes part of our belief system.</li>
+    <li>Be careful wording your opinions. Be truthful and loving when you put your opinions into words.</li>
+    <li>Gossip is like a virus that you inject to other's brains. Don't be the person that injects the virus. Always be ready to fight the virus injected to your brain.</li>
+    <li>How much you love yourself and how you feel about yourself are directly proportionate to the quality and integrity of your word.</li>
+    <li>Use the word to share the love.</li>
+</ul>
+<h3>Don't Take Anything Personally</h3>
+<ul>
+    <li>Whatever happens around you, don't take it personally.</li>
+    <li>Personal importance or taking things personally is the maximum expression of selfishness because we make the assumption that everything is about us. Nothing other people do is because of you. It's because of themselves.</li>
+    <li>By taking things personal, you eat all their emotional garbage and now it becomes your garbage.</li>
+    <li>Even the opinions you have about yourself are not necessarily true. Therefore, don't take whatever you hear in your own mind personal.</li>
+    <li>Don't expect people to tell you the truth because they also lie to themselves.</li>
+</ul>
+<h3>Don't Make Assumptions</h3>
+<ul>
+    <li>Instead of making assumptions, ask questions.</li>
+    <li>If you don't understand, ask.</li>
+    <li>Have courage to ask questions.</li>
+    <li>THe sadness and drama in your life is rooted in making assumptions and takings things personally.</li>
+</ul>
+<h3>Always Do Your Best</h3>
+<ul>
+    <li>Under any circumstance, always do your best, no more and no less.</li>
+    <li>Your best is never going to be the same from one moment to the next, accept this.</li>
+    <li>To do your best, love what you're doing.</li>
+    <li>You'll make mistakes and there's nothing wrong about it. By doing your best, mistakes will become less frequent in time.</li>
+    <li>By doing your best and keep doing it, you become a master.</li>
+    <li>Just live one day at a time and do your best today.</li>
+</ul>
+
+<x-join-conversation href="https://twitter.com/merthurturk/status/1747518035752095918"/>
