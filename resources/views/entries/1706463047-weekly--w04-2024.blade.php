a5ba953ae0e19f925bfdaf3c91eaebccfc64abfc
Mert Hurturk <mert.hurturk@gmail.com>
2024-01-28 21:10:12 +0300

    Published my weekly review

---
+++ resources/views/entries/1706463047-weekly--w04-2024.blade.php
@@ -0,0 +1,25 @@
+<h1>W04 2024</h1>
+<p class="lead">2024 Jan 22 - 28 &bullet; Year progress: 8%</p>
+
+<h3>Highlights</h3>
+<ul>
+    <li>Until you cover the basics, all the optimization talk (or the nuance talk, or the “best way to...” talk) is forbidden… <a href="https://twitter.com/hubermanrules/status/1749060193474756706" target="_blank">Attia's Rule</a></li>
+    <li>An insight struck me this week: I'm sailing smoothly in most areas of my life, yet I find myself clinging to a survival mindset. It's a reminder of a gem I picked up from <a href="/entries/books/be-useful">'Be Useful'</a> &ndash; 'Your mindset determines your reality.' It's amazing how true that is!</li>
+</ul>
+
+<h3>During the week</h3>
+<ul>
+    <li><a href="https://music.apple.com/tr/album/loss-of-hope/1709190472?i=1709190488" target="_blank">Meute - Loss of Hope</a></li>
+    <li>The other day, my daughter and I were chatting, and she hit a bit of a snag with a sentence in her book. It just wasn't making sense to her. So, we brainstormed this fun idea: What if every sentence was like a person you could talk to? Imagine some sentences being grumpy and short-tempered, while others are friendly and giving. Instead of guessing what they mean, we thought about asking them questions, sort of having a little chat with them. This way, their true meanings might just come out in the conversation!</li>
+    <li>Check out this <a href="https://www.simplethread.com/20-things-ive-learned-in-my-20-years-as-a-software-engineer/" target="_blank">fantastic compilation of insights</a> from 20 years in the software engineering field. It's impressive how skilled and dedicated professionals can distill their rich experiences into a list that's so accessible and easy to digest. Maybe I should start collecting all these people and their lists in one place?</li>
+</ul>
+
+<h3>Review</h3>
+
+<p>My week was beautifully lit up by a burst of inspiration from a chat with my daughter. It's about turning everything into a conversation – with sentences, with problems, with ideas, and even with thoughts. I'm excited to capture and document these moments, making sure I'm fully mindful and present in every experience.</p>
+
+<x-blockquote author="Unknown">
+    Discipline is the strongest form of self-love.
+</x-blockquote>
+
+<x-join-conversation href="https://twitter.com/merthurturk/status/1748992146919559260"/>
