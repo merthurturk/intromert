ef3ba9b32e535965ac7bf049f6411044e3f9cf1d
Mert Hurturk <mert.hurturk@gmail.com>
2024-01-28 19:05:31 +0300

    Added "Be Useful" book notes

---
+++ resources/views/entries/1706432480-books--be-useful.blade.php
@@ -0,0 +1,139 @@
+<h1>Be Useful</h1>
+
+<p class="lead">This is one of those books which gives me such excitement and energy that feels like I'm going to explode in-place. I've read this book after <a href="/entries/books/discipline-is-destiny">Discipline is Destiny</a> from Ryan Holiday, and they're a perfect match to be read one after another. Arnold Schwarzenegger doesn't bring new things to the table however, the way he represent the well established principles just clicked with me. Schwarzenegger's blend of timeless wisdom and personal anecdotes in 'Be Useful' not only complements the lessons in 'Discipline is Destiny' but also ignites a familiar fire within me, highlighting the transformative power of vision and relentless effort.</p>
+
+<x-blockquote author="Arnold Schwarzenegger, Be Useful: Seven Tools For Life">
+    What about time for rest and relaxation? First of all, rest is for babies and relaxation is for retired people.
+</x-blockquote>
+
+<x-image :src="asset('assets/images/be-useful.jpg')">
+    <a href="https://www.goodreads.com/book/show/125063314-be-useful" target="_blank">Be Useful: Seven Tools For Life, Arnold Schwarzenegger</a>.
+</x-image>
+
+<h3>Have a Clear Vision</h3>
+<ul>
+    <li>Everything that is good starts with a clear vision.</li>
+    <li>A clear vision gives you a way to decipher whether a decision is good or bad for you, based on whether it gets you closer or further away from where you want your life to go.</li>
+    <li>You can start small and build out until a big, clear picture reveals itself for you. Or you can start very broad and then, like the lens on a camera, zoom in until a clear picture snaps into focus.</li>
+    <li>First, create little goals for yourself. Don't worry about the big, broad stuff for now. Focus on making improvements and banking achievements one day at a time.</li>
+    <li>Start doing and completing things every day with a little goal attached to them, and then all of a sudden you will find yourself looking at things differently. Once you've developed a rhythm with those little daily goals, create weekly and then monthly goals.</li>
+    <li>Build momentum, create time and space every day to think, to day-dream, to look around, to be present in the world, to let inspiration and ideas in.</li>
+    <li>You need to SEE your vision. Visualize your goals.</li>
+    <li>As uncomfortable as it can be, you have to look at yourself in the mirror every day in order to know where you stand. You have to make sure that the person looking at you is the same one you see when you close your eyes and visualize the person you are trying to become.</li>
+    <li>You need to know whether your vision aligns with the reality of your choices.</li>
+    <li>The difference between the good ones and the bad ones is simple and obvious: it's self-awareness and clarity of vision. The good ones check in with themselves on a regular basis. Their vision changes as they change. It grows and evolves with them. The good ones aren't afraid of the mirror.</li>
+    <li>You can't grow unless you watch yourself do the work.</li>
+    <li>You can't get better unless you judge your effort against what you know it should look like, in your heart and in your mind.</li>
+</ul>
+<h3>Never Think Small</h3>
+<ul>
+    <li>Aim higher and always do your best.</li>
+    <li>If you only aim for the smaller goal, the big goals is automatically out of reach, in part because you are no longer motivated to truly go all in and focus on all the little things that make the difference between greatness and good enough.</li>
+    <li>...in the face of doubt and skepticism, they kept going.</li>
+    <li>Naysayers are a fact of life. That doesn't mean they get to have a say in your life.</li>
+    <li>Hear what naysayers have to say and then use it as motivation.</li>
+    <li>There is no plan B. Plan B is to succeed at plan A.</li>
+    <li>The second you create a backup plan, no only are you giving a voice to all the naysayers, but you are shrinking your own dream by acknowledging the validity of their doubts.</li>
+    <li>When you're thinking about your goals and crafting that vision for your life, you have to remember that it's not just about you. You could have a huge impact on the people around you. While you are breaking new ground in your own life, you could be blazing trails for people you didn't even know were watching.</li>
+</ul>
+<h3>Work You Ass Off</h3>
+<ul>
+    <li>There is no substitute for putting in the work.</li>
+    <li>Working your ass off is the only thing that works 100 percent of the time for 100 percent of the things worth achieving.</li>
+    <li>The key is, they have to be good reps. Not lazy, distracted, arched-back, noodle-arm, bullshit reps. You have to use proper form. You have to complete the entire exercise. You have to give maximum effort. It doesn't matter if we're talking about a dead lift, a press conference, or a run-through of an entire speech. You need to be all there, all in, every time.</li>
+    <li>The goal is to increase the load you're able to handle so that when it's time to do the work that matters, you don't have to think about whether you can do it. You just do it.</li>
+    <li>We have to embrace the boring stuff. We have to nail the fundamentals. We have to do them right, and we have to do them often. This is the only way we can build that strong base and all that muscle memory, so that performing when it counts isn't a question. It's the easy part.</li>
+    <li>To do great things that last, sacrifices are necessary.</li>
+    <li>That's the beauty of pain. Not only is it temporary, which means you don't have to deal with it forever, it tells you whether you've begun to give enough of yourself in pursuit of your dreams. Pain is a measure of growth potential.</li>
+    <li>Reps build strength, but pain builds size.</li>
+    <li>Muhammad Ali famously said that he didn't start counting his sit-ups until they hurt. "They're the only ones that count," he said. "That's what makes you a champion."</li>
+    <li>Nothing builds character like resilience or perseverance through pain. Nothing destroys character like succumbing to pain and quitting.</li>
+    <li>Pain only needs to have meaning to you for it to be bearable.</li>
+    <li>So many people are content to depend entirely on plans and systems, or to do the bare minimum asked of them, and then think to themselves, <em>This is all set, I took care of it.</em> No. Don't be a lazy fuck. Do the work. The only time you are allowed to use the phrase "I took care of it" is when it is done. Completely.</li>
+    <li>The person who is OK doing four sets of ten shitty half reps on the pull-down machine is more likely to sloppily change their baby's diaper or forget their partner's order at their favorite restaurant than the person who struggles through five sets of fifteen painful but perfect reps, even if it takes them longer and leaves them exhausted. Especially then, I would say, because those people know how good it feels to work hard and do things the right way.</li>
+    <li>If I'd focused completely on the end result or tried to swallow the elephant in one bite, as the saying goes, I absolutely would have choked. I would have failed. The only way to achieve the kind of sustainable, life-changing success that I wanted was to do the hard, incremental work day in and day out. I had to focus on doing the reps and executing well. I had to listen to the pain and build on the growth that would eventually come. I had to follow through, every day, on the plan I'd created in pursuit of my larger vision.</li>
+    <li>What about time for rest and relaxation? First of all, rest is for babies and relaxation is for retired people.</li>
+    <li>...it's work that you are more than capable of doing when you've planned it out and broken it down into little, daily goals that shouldn't take more than an hour or two to complete.</li>
+</ul>
+<x-blockquote author="Arnold Schwarzenegger, Be Useful: Seven Tools For Life">
+    Work works.
+</x-blockquote>
+<x-blockquote author="Arnold Schwarzenegger, Be Useful: Seven Tools For Life">
+    ...So don't tell me you don't have time to train, or to study, or to write, or to network, or to do whatever you need to do to achieve your vision. Turn your TV off. Throw your machines out the window. Save your excuses for someone who cares. Get to work.
+</x-blockquote>
+<h3>Sell, Sell, Sell</h3>
+<ul>
+    <li>You have to communicate and promote so that people know it exists. You need to sell it.</li>
+    <li>You can have the most amazing idea, the most fantastic plan, the best in class of virtually anything, but if nobody knows that it exists or knows what it is, then it's a waste of time and effort. It might as well not exist at all.</li>
+    <li>No one is better equipped or motivated than you to sell your vision to the world.</li>
+    <li>No matter the size of your dream, you have to know how to sell it and who to sell it to.</li>
+    <li>You are your first customer. The purpose of getting crystal clear on your vision and thinking about how it's going to happen is to sell yourself on the possibility of your own dream.</li>
+    <li>If you really want to supercharge your dream's exposure to the world, don't just tell them about it, act like it's already come true. You do that by talking openly about what you're working toward but removing the phrase "will be" from your vocabulary. It's not "I will be a great bodybuilder." It's "I can see myself as a great bodybuilder." It's not "I will be a leading man." It's "I can see myself as a leading man."</li>
+    <li>Before you can achieve your goals, I think you need to express them. Share them. I think you need to admin to yourself, and communicate to others, that this thing that started in your mind as a little idea has exploded into a massive dream with huge potential to benefit your life and theirs.</li>
+    <li>A good salesman knows that the key to making a sale and creating a customer for life is to give the customer more than they expected and leave them feeling like they're always getting the better end of the deal.</li>
+    <li>The best way to exceed expectations every time is to keep those expectations low for as long as possible.</li>
+    <li>Bridging is a communication technique that anyone can use to take control of a hostile discussion or to avoid a question you don't want to answer by shifting it toward a topic that better serves your agenda instead of the agenda of the person on the other side of the microphone or the negotiating table. The way you do this is to listen to the question being asked and then to start your response by accepting the premise of the question in order to establish common ground with your questioner. Once you've made them feel a little more comfortable by doing that, then you immediately pivot to re-frame the question and say whatever you want.</li>
+    <li>You don't want to resist the momentum of the people who are underestimating you. Instead, you want to use their momentum against them by grabbing a hold of it then pivoting and tossing their asses out of the ring. You want to bridge their bullshit right into the garbage where it belongs.</li>
+    <li>Embrace who you are! Own your story! Even if you don't like it. Even if it's bad, and you're ashamed.</li>
+    <li>Own your story, write it yourself, in your own words.</li>
+    <li>Nothing sells better than a true story from a genuine person. Especially when the story is about that person.</li>
+</ul>
+<h3>Shift Gears</h3>
+<ul>
+    <li>I have a rule: no complaining about a situation unless you're prepared to do something to make it better. If you see a problem, and you don't come to the table with a potential solution, I don't want to hear your whining about how bad it is. It couldn't be that bad if it hasn't motivated you to try to fix it.</li>
+    <li>Whatever your vision is, there is going to be a struggle. You have to get good at shifting hears and finding the positive in things. You have to know how to re-frame the failure you experience and understand the risks you're undertaking. Confronting problems instead of complaining about them gives you the chance to practice all these skills.</li>
+    <li>...focusing on all that negativity is a waste of time, because I don't just want to survive, I want to thrive...</li>
+    <li>Your mindset determines your reality, nothing else.</li>
+    <li>Anytime I find myself in a shitty situation and I feel that urge to bitch and moan rising up within me, I stop, take a breath, and tell myself that it's time to switch gears. I will actually talk to myself out loud and remind myself to look for the positive in my situation.</li>
+    <li>Set a goal and track your progress.</li>
+    <li>Eliminate thinking for progress, create systems.</li>
+    <li>It all starts by catching yourself any time you start to complain, then talking yourself into switching gears and looking for the good in things. If you can choose joy over jealousy, happiness over hate, love over resentment, positivity over negativity, then you have the tools to make the best of any situation, even one that feels like failure.</li>
+    <li>When you fail, learn from your failure and then say "I'll be back."</li>
+    <li>Failure is not fatal. All positive talk about failure has become a cliché at this point, because we all know it's the truth.</li>
+    <li>You have to remember that your failure on that particular lift doesn't mean you've lost somehow. It actually means that your workout was a good one, that your muscles were fully fatigued. It means you did the work.</li>
+    <li>When failure is a positive part of the game you play, it's much less scary to search for the limits of your ability.</li>
+    <li>When it comes to achieving your vision, it isn't failure you have to worry about, it's giving up.</li>
+    <li>No one who has set a world record, or started a successful business, or set the high score on a video game, or done anything difficult at all that they cared about, has been a quitter.</li>
+    <li>As you think about this thing you want to do, or the mark you want to make in this world, remember that your job is neither to avoid failure nor to seek it out. Your job is to bust your ass in pursuit of your vision&mdash;yours and nobody else's&mdash;and to embrace the failure that is bound to come.</li>
+    <li>Failure teaches you what doesn't work and points you toward the things that do.</li>
+    <li>"It's how things have always been done." When I hear those words, I see red. What really pisses me off is when the people who are doing the new things being objected to accept that seven-word status quo and give up.</li>
+    <li>Get on board or get out of the way, and if you do neither, expect to get worked around, or run over. Is there a risk in taking that kind of approach to achieving your ultimate vision? Possibly... I would argue that doing whatever you need to do to make your dreams a reality and to create the life you want for yourself is worth the risk.</li>
+    <li>Risk is just the name we give to the conclusion that each one of us comes to independently when we evaluate a choice for its chances of success compared to the consequences of its failure.</li>
+    <li>When you want something bad enough, and it means enough to you, at some point you have to be willing to reach for the brass ring and not give a fuck about it anymore.</li>
+    <li>The worst thing that can happen when you do the work to overcome adversity instead of quit in the face of it is that you fail again and learn one more way that doesn't work. Then that just forces you to switch gears, which gets you one step closer to your goal, because now you're more likely to be heading in the right direction.</li>
+</ul>
+<x-blockquote author="Arnold Schwarzenegger, Be Useful: Seven Tools For Life">
+    People who lack vision are threatened by those who have it.
+</x-blockquote>
+<x-blockquote author="Arnold Schwarzenegger, Be Useful: Seven Tools For Life">
+    My vision from the very beginning was to be the best bodybuilder, then the biggest star, then to help the most people. Not someday, not eventually, but as soon as I possibly could.
+</x-blockquote>
+<h3>Shut Your Mouth, Open Your Mind</h3>
+<ul>
+    <li>Renaissance man: one who has interest in learning and has focus on being open to new things.</li>
+    <li>If we wanted to be successful in anything we chose to do, at any point in our lives, young or old, and if we wanted to maximize our potential and our opportunities, then we needed to have a good head on our shoulders and an active mind.</li>
+    <li>When you're curious, and you're humble enough to admit that you don't know everything, people like that want to talk to you. They want to help you. Your curiosity and humility show them you don't have too much of an ego to listen to them.</li>
+    <li>Having the patience and humility to listen well is an essential ingredient of curiosity, and it's the secret to learning.</li>
+    <li>You don't know as much as you think you do, so shut your mouth and open your mind.</li>
+    <li>People are resources. But it's only when you learn to soak up what those people tell you that you truly begin to make yourself useful to others and become a resource yourself.</li>
+    <li>Use it or lose it. These words apply to so many areas of life, they should be considered a law of the universe.</li>
+    <li>If you don't regularly flex your mind like a muscle and put your knowledge to work, it will eventually lose its power.</li>
+</ul>
+<x-blockquote author="Dalai Lama">
+    When you talk, you are only repeating what you already know. But if you listen, you may learn something new.
+</x-blockquote>
+<h3>Break Your Mirrors</h3>
+<ul>
+    <li>It's important for you to recognize that you have a responsibility to give back. To help others. To send the ladder back down and lift the next group up. To pay it forward. To be useful. When you fully embrace that responsibility, it will change your life and improve the lives of countless others.</li>
+    <li>Life isn't zero-sum. We can all grow together, get richer together, get stronger together. Everyone can win, in their own time, in their own way.</li>
+    <li>By being selfless, by helping your costar or your competitor or your colleague, you have the ability to make everybody's life better and to create a positive environment where you can thrive and find happiness as well.</li>
+    <li>It's not about the amount that you give, it's about the fact that you give at all. It's the act of giving that produces the increased happiness.</li>
+    <li>Helping others is a simple practice that requires nothing more than awareness, willingness, and a little bit of effort.</li>
+    <li>Everyone has something to give.</li>
+    <li>If you're still struggling to think up ways you can give back, don't focus on what you have or what you know, take a personal inventory of what others have done for you in your life and try to pay it forward by doing those same things for others who might be in a similar situation.</li>
+    <li>...make use of your time, skills, and resources for the benefit of others.</li>
+    <li>...break your mirrors and do for others what you are able to do.</li>
+    <li>In every case, giving more will get you more. Want to help yourself? Help others. Learn to start from that place, and that is how you will become the most useful version of yourself&mdash;to your family, to your friends, to your community, to your nation...and to the world.</li>
+</ul>
+
+<x-join-conversation href="https://twitter.com/merthurturk/status/1748647662750969941"/>
