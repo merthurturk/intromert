f69c0b2a9e5ff64c84fbd21f1a612e168648d6ad
Mert Hurturk <mert.hurturk@gmail.com>
2024-02-04 14:06:37 +0300

    Published W5-2024 review

---
+++ resources/views/entries/1707041965-weekly--w05-2024.blade.php
@@ -0,0 +1,27 @@
+<h1>W05 2024</h1>
+<p class="lead">2024 Jan 22 - 28 &bullet; Year progress: 10%</p>
+
+<h3>Highlights</h3>
+<ul>
+    <li>Started reading <a href="https://www.goodreads.com/book/show/32768424-mindset" target="_blank">Mindset</a> from Carol S. Dweck. I always thought that I had a growth-mindset but after reading the first two chapters of this book, I learned that we can have different mindsets in different areas of life, and I realized that I have a fixed-mindset in several areas of my life.</li>
+    <li>My garden gym is ready (a bench press and a few dumbbells), and starting from tomorrow I will be tracking my workouts. I will be sharing more about my progress.</li>
+    <li>In just two weeks, I've managed to build a POC for an email builder on top of <a href="https://tiptap.dev" target="_blank">Tiptap</a> with support for templates and themes, and <a href="https://mjml.io" target="_blank">MJML</a>. It was very challenging but at the same time very rewarding. I needed this.</li>
+</ul>
+
+<h3>During the week</h3>
+<ul>
+    <li>I found myself frustrated with how teachers share links online with kids for homework and exercises without any content supervision and without policies in place to ensure that kids are not exposed to harmful content. But then, I stopped and thought, 'Frustration and judgment will not solve this issue.' Of course, being a software developer pushes me to solve problems by developing software, but my good friend <a href="https://www.linkedin.com/in/emreatmaca" target="_blank">Emre</a> gave me another idea: policy-making. I will be thinking more on this in the upcoming weeks.</li>
+    <li>In every action I take, I've started to remind myself continuously: "Be a good husband, be a good friend, be a good parent, be a good colleague, be a good leader, be a good person, work hard, stay physically fit, get enough sleep, read and learn." and yes, I believe I can do it all...</li>
+    <li>From my call with <a href="https://twitter.com/AlexAndBooks_" target="_blank">Alex</a>, he reminded me: "It's the actions you're taking, not the notes from the book, that make you a good reader." &mdash; Less notes, more action.</li>
+    <li>A few years ago, I had an idea for a series of drawings, which collected dust on the shelves of forgotten realms. :) I've decided to give it another go, but this time I will work with ChatGPT to develop the idea and come up with an execution plan. I will be sharing the progress here, on my website.</li>
+</ul>
+
+<h3>Review</h3>
+
+<p>I love new beginnings and the thrill of momentum... This week was dedicated to embracing new starts and propelling momentum. Next week, my plan is simply to add more wood to the fire.</p>
+
+<x-blockquote author="Dale Carnegie">
+    Inaction breeds doubt and fear. Action breeds confidence and courage.
+</x-blockquote>
+
+<x-join-conversation href="https://twitter.com/merthurturk/status/1751670014162116752"/>
