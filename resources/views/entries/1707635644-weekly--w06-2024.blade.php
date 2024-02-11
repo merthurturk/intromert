9aab5bdc3f2996e83fd9525f748113a1604db984
Mert Hurturk <mert.hurturk@gmail.com>
2024-02-11 13:28:44 +0300

    updated w06 discussion link

--- resources/views/entries/1707635644-weekly--w06-2024.blade.php
+++ resources/views/entries/1707635644-weekly--w06-2024.blade.php
@@ -10,7 +10,7 @@
 <h3>During the week</h3>
 <ul>
     <li>Putting in your utmost effort and every ounce of your energy counts as a true victory. This week, I reminded myself of the importance of focusing on the process rather than just the outcome. It's a tough shift, but a crucial one.</li>
-    <li>Learning through experimentation, documenting progress, dissecting failures and learning from them... KPI is "Key performance indicators"... Performance is more related with process rather than outcomes, but we keep coming up with outcome goals and use KPIs to track rather we reach them or not. What if we used KPIs as monitoring tool for the process and experimentation?</li>
+    <li>Learning through experimentation, documenting progress, dissecting failures and learning from them... KPI is "Key Performance Indicator"... Performance is more related with process rather than outcomes, but we keep coming up with outcome goals and use KPIs to track rather we reach them or not. What if we used KPIs as monitoring tool for the process and experimentation?</li>
     <li><a href="https://nesslabs.com/personal-experiments" target="_blank">Personal experiments</a></li>
     <li><a href="https://twitter.com/ryanholiday/status/1753538814080115144?s=12" target="_blank">7 rules for a journaling routine</a></li>
     <li><a href="https://vimeo.com/85040589" target="_blank">The Gap by Ira Glass</a></li>
@@ -24,4 +24,4 @@
     Nobody tells this to people who are beginners, I wish someone told me. All of us who do creative work, we get into it because we have good taste. But there is this gap. For the first couple years you make stuff, it’s just not that good. It’s trying to be good, it has potential, but it’s not. But your taste, the thing that got you into the game, is still killer. And your taste is why your work disappoints you. A lot of people never get past this phase, they quit. Most people I know who do interesting, creative work went through years of this. We know our work doesn’t have this special thing that we want it to have. We all go through this. And if you are just starting out or you are still in this phase, you gotta know its normal and the most important thing you can do is do a lot of work. Put yourself on a deadline so that every week you will finish one story. It is only by going through a volume of work that you will close that gap, and your work will be as good as your ambitions. And I took longer to figure out how to do this than anyone I’ve ever met. It’s gonna take awhile. It’s normal to take awhile. You’ve just gotta fight your way through.
 </x-blockquote>
 
-<x-join-conversation href="https://twitter.com/merthurturk/status/1754100506857803792"/>
+<x-join-conversation href="https://twitter.com/merthurturk/status/1756626083275796663"/>
