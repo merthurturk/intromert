5244853db6b6c2abfa6898d39853024986b0ea87
Mert Hurturk <mert.hurturk@gmail.com>
2024-02-18 19:12:18 +0300

    Published weekly review

---
+++ resources/views/entries/1708264228-weekly--w07-2024.blade.php
@@ -0,0 +1,37 @@
+<h1>W07 2024</h1>
+<p class="lead">2024 Feb 12 - 18 &bullet; Year progress: 13%</p>
+
+<h3>Highlights</h3>
+<ul>
+    <li>This week, I took a deep dive into upcoach's codebase, starting from scratch, as though I was completely new to the project. What was I aiming for? I wanted to spot where the friction points were and identify the weakest links in our developer experience. This approach was enlightening right off the bat, bringing to light areas that are screaming for improvement. And here's the thing – this isn't just about coding. This idea of stepping back and seeing things with fresh eyes? It works everywhere. Signing up as a newbie to see what your onboarding feels like, or having your team explain their processes as if you're hearing it for the first time, can reveal so much. It's about gaining those crucial insights that we might overlook when we're too close to the project.</li>
+    <li>
+        You aren't allowed to argue about supplements &amp; nutrition until you can do the following:
+        <ol>
+            <li>Deadlift your bodyweight for 10 reps</li>
+            <li>Wall sit for 2 minutes</li>
+            <li>Dead hang for 1 minute</li>
+            <li>Your VO2 max is in the 75th percentile (for your age and gender)</li>
+        </ol>
+        <a href="https://twitter.com/hubermanrules/status/1749060193474756706?s=20" target="_blank">Attia's rule</a><br>
+        <a href="/entries/weekly/w04-2024">Also mentioned in W04 review</a>
+    </li>
+    <li>Spent three days in Istanbul. The new places and people made me feel similar to when I restart my laptop - everything's fresh and fast now.</li>
+</ul>
+
+<h3>During the week</h3>
+<ul>
+    <li>Started setting up my personal dashboard on <a href="https://upcoach.com" target="_blank">upcoach</a> as a program. It has my vision board, projects, tasks and some inspiring quotes. More to come later. I'll keep sharing the progress on <a href="https://x.com/merthurturk">X</a></li>
+    <li>My family and I have read more in the last four weeks than we did in the entire last year, all thanks to <a href="https://twitter.com/AlexAndBooks_/status/1747264930376782060" target="_blank">Alex's amazing tip</a></li>
+    <li>Finished reading <a href="https://www.goodreads.com/book/show/58145670-courage-is-calling" target="_blank">Courage is Calling</a> from Ryan Holiday. <a href="/entries/books/courage-is-calling">Check out my highlights and notes here</a>.</li>
+    <li>Lots of new ideas and projects, amazing brainstorming session with my brother, <a href="https://cemhurturk.com" target="_blank">Cem</a>.</li>
+</ul>
+
+<h3>Review</h3>
+
+<p>I'm experiencing <a href="https://www.ncbi.nlm.nih.gov/pmc/articles/PMC7254162/#:~:text=It%20is%20a%20state%20of,being%20as%20a%20learning%20process." target="_blank">euthymia</a>, feeling certain about my direction and moving full throttle towards it.</p>
+
+<x-blockquote author="Henry David Thoreau">
+    If one advances confidently in the direction of his dreams, and endeavors to live the life which he has imagined, he will meet with a success unexpected in common hours.
+</x-blockquote>
+
+<x-join-conversation href="https://twitter.com/merthurturk/status/1756626083275796663"/>
