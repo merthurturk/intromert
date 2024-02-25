7f3432276038604dae43fb0c7922401fd91ec377
Mert Hurturk <mert.hurturk@gmail.com>
2024-02-25 20:32:46 +0300

    Week 8 of 2024 review published

---
+++ resources/views/entries/1708878597-weekly--w08-2024.blade.php
@@ -0,0 +1,39 @@
+<h1>W08 2024</h1>
+<p class="lead">2024 Feb 19 - 25 &bullet; Year progress: 15%</p>
+
+<h3>Highlights</h3>
+<ul>
+    <li>
+        <p>On Sunday, I spent an hour doing nothing but watching and listening to the sky and the sea.</p>
+
+        <video controls width="100%">
+            <source src="{{ asset('/assets/videos/february-bodrum-castle.mov') }}" type="video/quicktime"/>
+        </video>
+    </li>
+    <li><a href="https://upcoach.com" target="_blank">upcoach</a> team is doing amazing with our novel approach to product delivery. It's a mix of this and that, and we call it "The Pool". I will share more about this soon.</li>
+    <li>I finally did it—I bought the <a href="https://www.casio.com/tr/watches/gshock/product.GBD-H2000-1B/" target="_blank">Casio G-SHOCK G-SQUAD GBD-H2000-1B</a> and I'm thrilled with it so far. After several years of relying on my Apple Watch, which I'll still wear from time to time, the shift feels liberating. Not having to constantly worry about the battery charge is a breath of fresh air. While there are a few more Casios on my wishlist, for now, this purchase feels more than sufficient.</li>
+</ul>
+
+<h3>During the week</h3>
+<ul>
+    <li>
+        I’ve returned to my passion for drawing and have started sharing my drawings. This week, I've posted 5 drawings from my notebook and plan to continue doing so until I've filled its pages. This notebook is a cherished gift from my dear friend and partner at <a href="https://upcoach.com" target="_blank">upcoach.com</a>, <a href="https://www.davidhenzel.com" target="_blank">David Henzel</a>.
+        <a href="https://twitter.com/merthurturk/status/1760019524223426961" target="_blank">1</a>,
+        <a href="https://twitter.com/merthurturk/status/1760333204836208945" target="_blank">2</a>,
+        <a href="https://twitter.com/merthurturk/status/1760707608812351831" target="_blank">3</a>,
+        <a href="https://twitter.com/merthurturk/status/1760950343200870433" target="_blank">4</a>,
+        <a href="https://twitter.com/merthurturk/status/1761420606476628456" target="_blank">5</a>
+    </li>
+    <li>After a several-week hiatus, I resumed pushing code to production on <a href="https://upcoach.com" target="_blank">upcoach.com</a>, and it feels amazing! I have immense love and appreciation for our team at upcoach.</li>
+    <li>My dear friend <a href="x.com/fahrio" target="_blank">Fahri</a> is crafting <a href="https://hyper.place">Hyper.place</a>, and the progress so far is incredible. Here's one 'hyper place' he shared with me: <a href="https://hyper.place/places/iixc41i7zo7m" target="_blank">Caspar David Friedrich</a>. Although I have access to a few more, I'll wait for him to unveil them first ;)</li>
+</ul>
+
+<h3>Review</h3>
+
+<p>Filled with drawing, coding, and unstoppable momentum... If every week of this year mirrored this one, it would truly be a dream come true. Let's keep this energy going!</p>
+
+<x-blockquote author="Carl Jung">
+    You are what you do, not what you say you'll do.
+</x-blockquote>
+
+<x-join-conversation href="https://twitter.com/merthurturk/status/1759252522533839190"/>
