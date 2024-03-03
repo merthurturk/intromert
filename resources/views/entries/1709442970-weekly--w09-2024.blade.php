7c4f826aac6aa8be8bde05faf9f510790208c21b
Mert Hurturk <mert.hurturk@gmail.com>
2024-03-03 10:01:00 +0300

    Weekly review published

---
+++ resources/views/entries/1709442970-weekly--w09-2024.blade.php
@@ -0,0 +1,36 @@
+<h1>W09 2024</h1>
+<p class="lead">2024 Feb 26 - March 3 &bullet; Year progress: 17%</p>
+
+<h3>Highlights</h3>
+<ul>
+    <li>
+        Every time I jot down some notes and ideas before I start drawing (even if they mostly seem like a jumble of thoughts), the outcome is almost always surprisingly better.
+
+        <div class="relative md:-mx-20">
+            <div class="flex flex-row items-center space-x-2 md:space-x-10">
+                <img src="{{ asset('assets/images/notes-to-drawing-01.jpeg') }}" alt="" width="42%">
+                <div style="width: 6%;">&rarr;</div>
+                <img src="{{ asset('assets/images/notes-to-drawing-02.jpeg') }}" alt="" width="52%">
+            </div>
+        </div>
+    </li>
+    <li>Weekly Gratitude: If someone had come up to me 20 years ago and told me that one day, I'd be living in a home filled with drums, a bunch of guitars, a piano, and a cello, with each being played and loved by my family (though not everyone plays every instrument), I would've chuckled and said, 'That’s the dream!' And you know what? That dream actually did come true. I'm super grateful for all the effort the younger me put in and for my family's incredible support and zest for music.</li>
+</ul>
+
+<h3>During the week</h3>
+<ul>
+    <li>An amazing personal story from Michael Caine on his life philosophy: <a href="https://youtu.be/GAB89fOdA-I">Michael Caine - Use The Difficulty</a> &mdash; <a href="https://twitter.com/sahilbloom/status/1755929653049545041?s=12" target="_blank">source</a></li>
+    <li>Shipped a new feature on <a href="https://upcoach.com" target="_blank">upcoach</a> and still, it feels amazing to ship things to production, a different kind of joy for sure...</li>
+    <li>I've noticed something interesting - and I'm not sure if it's the same elsewhere - but here in Turkey, talking about personal finance seems to be a bit of a taboo. It's like, even within families, there's this silence about money matters. Parents often keep their financial decisions, their wins and their losses, to themselves. It makes you wonder, without sharing knowledge and experiences openly, how can anyone expect to grow? Recently, a good friend and I had this eye-opening chat about it all. It really got me thinking about sharing my own journey and what I'm learning (still) about handling finances as a family of four. I'm still figuring out the best way to do this, but I'm eager to start the conversation.</li>
+    <li><a href="https://www.instagram.com/reel/C1vaZLPxxRW/?igsh=MXE3ZnM2d3lqMHp6eQ%3D%3D" target="_blank">You think a couple bad days is going to stop me? Ha!</a></li>
+</ul>
+
+<h3>Review</h3>
+
+<p>Despite hitting a slump on Thursday and Friday, I still managed to end the week on a high note. The momentum is steady, and I'm doing everything I can to keep it that way.</p>
+
+<x-blockquote author="Rick Rubin">
+    Never say you can't do it. Say I haven't done it yet.
+</x-blockquote>
+
+<x-join-conversation href="https://twitter.com/merthurturk/status/1761808330912117081"/>
