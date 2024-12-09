1168334a6499e11b85484430652af05edc869361
Mert Hurturk <mert.hurturk@gmail.com>
2024-12-09 22:32:03 +0300

    w49 2024 update

---
+++ resources/views/entries/1733769911-weekly--w49-2024.blade.php
@@ -0,0 +1,33 @@
+<h1>W49 2024</h1>
+<p class="lead">2024 Dec 2 - 8 &bullet; Year progress: 94%</p>
+
+<h3>Highlights</h3>
+<ul>
+    <li>
+        The <a href="https://upcoach.com" target="_blank">upcoach</a> team made impressive progress this week. We tackled major tasks for the upcoming website revamp and prepared for the Product Hunt launch of our new online payments feature. My 1-on-1s team members were energizing. I’m genuinely excited about what we’ll roll out in the coming weeks.
+    </li>
+    <li>
+        Meanwhile, <a href="https://sumbox.io" target="_blank">SumBox</a>, , which launched last week, has received positive feedback from its early users, even though they’re just a few for now. It brought me back to my early days of product building—the excitement of shipping something new and watching it come to life. It’s also been a reminder to adopt a swifter pace in executing and launching projects. As I plan the next sprint for SumBox improvements, I’ve already started working on a new micro-SaaS idea: WeekVista. It’s still early, but I’m excited to share more about it soon.
+    </li>
+    <li>
+        Finally, the weekend delivered a personal highlight—a father-daughter day filled with connection and capped off by some quiet time at a book café. Moments like these remind me of the joy of simply being in the moment.
+    </li>
+</ul>
+
+<h3>During the week</h3>
+<ul>
+    <li>The week wasn’t without its challenges. Navigating tough conversations was emotionally draining, yet they served as powerful reminders of the weight of leadership and the value of accountability. It’s in moments like these that I find opportunities to grow and fortify my resolve.</li>
+    <li>My productivity has been firing on all cylinders lately, but my routines and sleep? Not so much. It’s all a balancing act, I suppose, and for now, I’m okay with the trade-off.</li>
+</ul>
+
+<h3>Review</h3>
+
+<p>Despite some slow starts and moments of fatigue, I’m proud of how the week shaped up. Launching SumBox, progress on WeekVista, productive sessions with the upcoach team, and personal wins made it fulfilling. The momentum is steady, and I’m ready to keep pushing forward.</p>
+
+<p>Balance is not about perfection but about making the most of what matters—whether it’s shipping a feature, brainstorming a new idea, or cherishing time with family. This week was a testament to that.</p>
+
+<x-blockquote author="Unknown">
+    Progress over perfection.
+</x-blockquote>
+
+<x-join-conversation href="https://twitter.com/merthurturk/status/1764186958966755439"/>
