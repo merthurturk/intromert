7c583239846f43ceb41bc9ca9c205891df05d6cf
Mert Hurturk <mert.hurturk@gmail.com>
2024-12-15 14:30:02 +0300

    W50 2024 updates

---
+++ resources/views/entries/1734261426-weekly--w50-2024.blade.php
@@ -0,0 +1,29 @@
+<h1>W50 2024</h1>
+<p class="lead">2024 Dec 9 - 15 &bullet; Year progress: 96%</p>
+
+<h3>Highlights</h3>
+<ul>
+    <li><a href="https://upcoach.com" target="_blank">upcoach’s</a> Evolution: This week brought another round of progress with the upcoming website revamp. The team doing an amazing job with frequent impactful updates.</li>
+    <li><a href="https://sumbox.io" target="_blank">SumBox</a> Sprint Planning: I outlined the next major sprint for SumBox, focusing on usability improvements and refining product visuals. This sprint will run until year-end, aiming to deliver impactful updates just in time for 2025.</li>
+    <li><a href="https://weekvista.com">WeekVista</a> Milestone: <a href="https://www.producthunt.com/products/weekvista" target="_blank">The Product Hunt launch for WeekVista</a> is officially scheduled for January 10, 2025! The platform is evolving steadily, with early planning turning into tangible progress.</li>
+</ul>
+
+<h3>During the week</h3>
+<ul>
+    <li>The week was a mixed bag emotionally. Early wins, like the progress on upcoach's new website, scheduling the WeekVista launch and refining strategies for SumBox, were tempered by introspective moments. I had some though conversations, where I shared my struggles. It was a moment of vulnerability but also a reaffirmation of my commitment to transparency and fairness in everything I do.</li>
+    <li>On a brighter note, I stumbled upon my old 2019 notebooks and found my 5-year goals. It was an enlightening moment to see that I had completed 3 out of 5 items on the list. While they didn’t turn out exactly as I had envisioned, it reminded me of the power of setting intentions—even when they lack specificity. Inspired by this, I’ve begun crafting my 2025 vision and next 5-year plans. The process has been eye-opening, energizing, and just plain fun. Exercises like this are why I love reflecting and planning—it’s incredible to see how far I’ve come and where I want to go.
+        <strong>This time, I’ll make sure to be more specific. ;)</strong></li>
+    <li>Productivity-wise, I found myself in a balancing act. While some days felt like a blur of focused energy, others were slowed by self-doubt and distraction. Reflecting on my values helped me recenter—reminding me that tough times are part of the journey.</li>
+</ul>
+
+<h3>Review</h3>
+
+<p>Looking back, this week wasn’t perfect, but it was meaningful. Wins like launching ideas into action (upcoach, SumBox and WeekVista), engaging deeply with my team and people I'm working with, and rediscovering my long-term vision balanced out the challenges. My key takeaway? Staying true to my core values makes even the hardest days worthwhile.</p>
+
+<p>Balance is not about perfection but about making the most of what matters—whether it’s shipping a feature, brainstorming a new idea, or cherishing time with family. This week was a testament to that.</p>
+
+<x-blockquote author="Salvador Dali">
+    Have no fear of perfection &mdash; you’ll never reach it.
+</x-blockquote>
+
+<x-join-conversation href="https://x.com/merthurturk/status/1866205354201120969"/>
