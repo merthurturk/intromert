22e8bacb33162b243fec795296987d466b6496f9
Mert Hurturk <mert.hurturk@gmail.com>
2025-02-02 19:17:04 +0300

    added new sketchbook

--- resources/views/entries/1709442970-projects--crafting-time.blade.php
+++
@@ -1,7 +0,0 @@
-<h1>Crafting Time: The Journey of Building a Daily Planner and Time-Blocking App</h1>
-<p class="lead">I'm building a daily planner and time-blocking third-party app on <a href="https://upcoach.com" target="_blank">upcoach.com</a>, using its tasks and third-party app layer as a foundation to build upon.</p>
-
-<p>...</p>
-
-
-<x-join-conversation href="https://twitter.com/merthurturk/status/1761808330912117081"/>
---
+++ resources/views/entries/1731599223-sketchbooks--anything-goes-everything-stays.blade.php
@@ -0,0 +1,4 @@
+<x-sketchbook-layout slug="anything-goes-everything-stays">
+    <h1>Anything Goes, Everything Stays</h1>
+    <p>This sketchbook started with a simple goal: to allow everything without judgment, creating a space where I could finally return to sketching after a break. As I filled its pages, I realized that this acceptance extended beyond the paper—it was about embracing myself. I learned to honor the gap between my skill and my taste, no longer measuring it as a flaw, but as a space where growth and creativity coexist. By the end, it wasn’t just a collection of sketches but a quiet acknowledgment of self-acceptance.</p>
+</x-sketchbook-layout>
