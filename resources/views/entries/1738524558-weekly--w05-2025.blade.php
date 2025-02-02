4efba2504dba282a76a3faf3d6f4357913505d4c
Mert Hurturk <mert.hurturk@gmail.com>
2025-02-02 23:04:02 +0300

    Weekly update

---
+++ resources/views/entries/1738524558-weekly--w05-2025.blade.php
@@ -0,0 +1,25 @@
+<h1>W05 2025</h1>
+<p class="lead">2025 Jan 27 - Feb 2 &bullet; Year progress: 9%</p>
+
+<h3>Highlights</h3>
+<ul>
+    <li><a href="https://upcoach.com" target="_blank">upcoach</a>: Lots of progress on new features. Exciting updates coming in the next two months!</li>
+    <li><a href="https://x.com/merthurturk">@merthurturk</a> on X: Started sharing end-of-day reports. Not sure if I'll keep it up, but for now it helps me stay aware of time.</li>
+    <li>Family trip to Ekişehir: Such a vibrant, walkable city—full of energy! Always a great place for a family getaway.</li>
+</ul>
+
+<h3>During the week</h3>
+<ul>
+    <li>After ~18 challenging months, I finally felt my energy returning. This week was a hard reset—rebuilding my systems and routines.</li>
+    <li>Back to coding at upcoach! I built it from scratch 6 years ago and now, being hands-on again with an amazing team, I feel more connected than ever.</li>
+    <li>upcoach product team retreat in Istanbul → One intense, productive day with the team&mdash;huge progress!</li>
+    <li>After 10 days of travel, I’m back home, recharged, and ready to push forward.</li>
+</ul>
+
+<h3>Review</h3>
+
+<p>This feels like a fresh start. Even though I’m a bit under the weather, the energy is there. Next week is all about: Pushing forward, Tracking progress, Reflecting and adjusting.</p>
+
+<x-blockquote author="Benjamin Franklin">
+    Energy and persistence conquer all things.
+</x-blockquote>
